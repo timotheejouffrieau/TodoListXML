8378708f8229310e3b6314682b8bf3b866df21cb
Timothée Jouffrieau <timothee.jouffrieau.ext@inetum.com>
2023-10-04 13:18:03 +0200

    Create new activity and navigate to it

--- app/src/main/java/com/example/todolistxml/MainActivity.kt
+++ app/src/main/java/com/example/todolistxml/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.todolistxml
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,6 +37,13 @@ class MainActivity : AppCompatActivity() {
 
         taskListView.layoutManager = LinearLayoutManager(this)
         taskListView.adapter = taskAdapter
+
+        val addTaskButton = findViewById<FloatingActionButton>(R.id.addTaskButton)
+
+        addTaskButton.setOnClickListener {
+            val intent = Intent(this, EditTaskActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
