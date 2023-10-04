7766e54205265f6093a1a40c09cf73f97acd7723
Timothée Jouffrieau <timothee.jouffrieau.ext@inetum.com>
2023-10-04 11:08:32 +0200

    Create button and change text

--- app/src/main/java/com/example/todolistxml/MainActivity.kt
+++ app/src/main/java/com/example/todolistxml/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.todolistxml
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var compteur = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //Associe l'activité à la vue globale
+
+        val textView: TextView = findViewById(R.id.textView) //Récupère la vue associé au texte
+        val button: Button = findViewById(R.id.button) //Récupère la vue associé au bouton
+
+        button.setOnClickListener {//Défini l'action à faire après un click
+            compteur++ //Incrémente le compteur
+            textView.text = "Vous avez appuyer $compteur fois sur le bouton !" //Change le texte à afficher dans la vue texte en fonction du compteur
+            //textView.rotation = (compteur * 10).toFloat() //Fait évoluer la rotation du texte en fonction de la valeur du compteur
+        }
     }
 }
