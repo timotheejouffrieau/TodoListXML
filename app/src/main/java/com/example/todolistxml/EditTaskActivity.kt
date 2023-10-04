b5269e5271c057a423f65eeac6d47e335b81875d
Timothée Jouffrieau <timothee.jouffrieau.ext@inetum.com>
2023-10-04 15:52:41 +0200

    Implement function to catch new data

--- app/src/main/java/com/example/todolistxml/EditTaskActivity.kt
+++ app/src/main/java/com/example/todolistxml/EditTaskActivity.kt
@@ -1,11 +1,70 @@
 package com.example.todolistxml
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 
 class EditTaskActivity : AppCompatActivity() {
+
+    private val TAG = "EditTaskActivity"
+
+    private lateinit var currentTask: Task
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_task)
+
+        currentTask = Task(1)
+
+        val titleEditText = findViewById<EditText>(R.id.titleTaskEditText)
+        val contentEditText = findViewById<EditText>(R.id.contentTaskEditText)
+        val prioritySpinner = findViewById<Spinner>(R.id.prioritySpinner)
+
+        titleEditText.setText(currentTask.title)
+        titleEditText.doOnTextChanged { text, _, _, _ ->
+            Log.d(TAG, "titleEditText text change to $text")
+            currentTask = currentTask.copy(title = text.toString())
+        }
+
+        contentEditText.setText(currentTask.content)
+        contentEditText.doOnTextChanged { text, _, _, _ ->
+            Log.d(TAG, "contentEditText text change to $text")
+            currentTask = currentTask.copy(content = text.toString())
+        }
+
+        val listPriority = Priority.values()
+        prioritySpinner.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            listPriority.map { it.name } //transforme la liste de priorité en list de String
+        )
+        prioritySpinner.setSelection(listPriority.indexOf(currentTask.priority))
+
+        prioritySpinner.onItemSelectedListener = object :
+            OnItemSelectedListener { //définition d'un classe anonyme -> https://kotlinlang.org/docs/object-declarations.html
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.d(TAG, "prioritySpinner selection change to ${listPriority[position].name}")
+                currentTask = currentTask.copy(priority = listPriority[position])
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //Override obligatoire
+                //Mais on ne fait rien
+            }
+
+        }
+
     }
 }
